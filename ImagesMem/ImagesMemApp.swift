baa7ce5d0d6a0bad6aeb1c27c5203ac12ed1b0e8
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2024-11-27 14:20:04 -0300

    Initial Commit

---
+++ ImagesMem/ImagesMemApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagesMemApp.swift
+//  ImagesMem
+//
+//  Created by Marcelo Sampaio on 27/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ImagesMemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
